33452da13f2c30c910bac716e1abb9f6b4d2d9bd
Loïc <loic@webismymind.be>
2017-09-27 00:45:37 +0200

    syntax

--- Components/Loader.js
+++ Components/Loader.js
@@ -31,7 +31,7 @@ export default class extends Component {
                     easing: Easing.linear
                 }
             ).start(() => { runAnimation()});
-        }
+        };
         runAnimation();
     }
 
